32944fafde0130de28fe0df0d817effcd6b4dea8
Aditya Kshirsagar <kaditya.anil@gmail.com>
2022-06-03 22:45:54 +0530

    added exception handling

--- microphoneclass.py
+++ microphoneclass.py
@@ -15,9 +15,11 @@ mic = sr.Microphone()
 
 
 #Using listen to capture microphone input
-with mic as source:
-    r.adjust_for_ambient_noise(source)   # Handling ambient noise
-    audio = r.listen(source)
-
-text=r.recognize_google(audio)
-print(text)
+try:
+    with mic as source:
+        r.adjust_for_ambient_noise(source)   # Handling ambient noise
+        audio = r.listen(source)
+        text=r.recognize_google(audio)
+        print(text)
+except sr.UnknownValueError as error:
+    print("Sorry, didn't get that.")
