63c260989a053230bf10e3c2a5c088fdea17c5e2
Aditya Kshirsagar <kaditya.anil@gmail.com>
2022-06-03 22:41:11 +0530

    added microsphoneclass.py

---
+++ microphoneclass.py
@@ -0,0 +1,23 @@
+#Install pyaudio
+
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+# This is how you use default mic
+mic = sr.Microphone()
+
+# To see which other mics are available
+# print(sr.Microphone.list_microphone_names())
+
+# To select third from the ones listed above
+# sr.Microphone(device_index=3)
+
+
+#Using listen to capture microphone input
+with mic as source:
+    r.adjust_for_ambient_noise(source)   # Handling ambient noise
+    audio = r.listen(source)
+
+text=r.recognize_google(audio)
+print(text)
